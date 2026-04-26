2f1717bc2ef093ee57c62661c9d678cdb1313fff
brapinedao <162193611+brapinedao@users.noreply.github.com>
2026-04-26 14:58:58 -0500

    solucion de build render

--- vite.config.ts
+++ vite.config.ts
@@ -7,16 +7,19 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [
     vue({ template: { transformAssetUrls } }),
     vueJsx(),
-    vueDevTools(),
+    mode !== 'production' && vueDevTools(),
     quasar({ sassVariables: false }),
   ],
+  build: {
+    chunkSizeWarningLimit: 1000,
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-})
+}))
